30ca6be11911b4615c25d84e05328d9ed5306126
Tom Chauvel <tom.chauvel@hotmail.com>
2023-08-04 01:51:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ lat = "48.1160682"
 lon = "-1.6401422"
 
 url = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+token+"&units=metric";
-meteo = requests.get(url).json()
+
+req = requests.get(url)
+
+if req.status_code != 200 : raise Exception("Api Météo Pété") 
+
+meteo = req.json()
 
 temperature = int(meteo["main"]["temp"])
 code = meteo["weather"][0]["icon"]
