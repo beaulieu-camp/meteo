f6e40490b4951f709e63fbc681a25aaf8c1ebd0e
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2023-08-03 21:21:55 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+import requests
+import sys
+
+token = sys.argv[-1]
+
+lat = "48.1160682"
+lon = "-1.6401422"
+
+url = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+token+"&units=metric";
+meteo = requests.get(url).json()
+
+print(meteo)
+
+temperature = int(meteo["main"]["temp"])
+code = meteo["weather"][0]["icon"]
+
+with open("./out/index.json","w+") as file:
+    file.write(json.dumps({"temperature":temperature,"code":code}))
